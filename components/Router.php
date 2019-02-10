39b29af1af666cf7c89b0cfe48ae57fa4cdc9dd4
LocmanMR <israfilov.ruslan2018@yandex.ru>
2019-02-10 22:07:27 +0300

    FirstJob

---
+++ components/Router.php
@@ -0,0 +1,60 @@
+<?php
+
+class Router
+{
+    private $routes;
+
+    public function __construct()
+    {
+        $routesPath = ROOT.'/config/routes.php';
+        $this->routes = include($routesPath);
+    }
+
+    private function getIRU():string
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
+
+    public function run()
+    {
+        //Получить строку запроса
+        $uri = $this->getIRU();
+
+        //проверить наличие такого запроса в routes.php
+        foreach ($this->routes as $uriPattern => $path) {
+
+            //Сравниваем $uriPattern и $uri
+            if (preg_match("~$uriPattern~", $uri)) {
+
+                //Определить какой контроллер и action обрабатывают запрос
+                $segments = explode('/', $path);
+
+                $controllerName = array_shift($segments).'Controller';
+                $controllerName = ucfirst($controllerName);
+
+                $actionName = 'action'.ucfirst(array_shift($segments));
+
+                //Подключить файл класса-контроллера
+                $controllerFile = ROOT.'/controllers/'.$controllerName.'.php';
+
+                if (file_exists($controllerFile)) {
+                    include_once($controllerFile);
+                }
+
+                //Создать объект, вызвать метод(action)
+                $controllerObject = new $controllerName;
+                $result = $controllerObject->$actionName();
+                if ($result != null) {
+                    break;
+                }
+            }
+        }
+        //Если совпадение сть, определить какой контроллер а action обрабатывают запрос
+
+        //Подключить файл класса-контроллера
+
+        //Создать объект, вызвать метод (т.к. action)
+    }
+}
